80321e5b2347ae9a092469c5d5c887ce1cbc67ec
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-04-14 00:21:52 +0200

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/braintree_spring_example.jar
---
+++ src/test/java/springexample/CheckoutControllerTest.java
@@ -0,0 +1,98 @@
+package springexample;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.File;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class CheckoutControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeClass
+    public static void setupConfig() {
+        File configFile = new File("config.properties");
+        try {
+            if(configFile.exists() && !configFile.isDirectory()) {
+                Application.gateway = BraintreeGatewayFactory.fromConfigFile(configFile);
+            } else {
+                Application.gateway = BraintreeGatewayFactory.fromConfigMapping(System.getenv());
+            }
+        } catch (NullPointerException e) {
+            System.err.println("Could not load Braintree configuration from config file or system environment.");
+        }
+    }
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void checkoutReturnsOK() throws Exception {
+        mockMvc.perform(get("/checkouts"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void rendersNewView() throws Exception {
+        mockMvc.perform(get("/checkouts"))
+            .andExpect(view().name("checkouts/new"))
+            .andExpect(model().hasNoErrors())
+            .andExpect(model().attributeExists("clientToken"));
+    }
+
+    @Test
+    public void rendersErrorsOnTransactionFailure() throws Exception {
+        mockMvc.perform(post("/checkouts")
+                .param("payment_method_nonce", "fake-valid-nonce")
+                .param("amount", "2000.00"))
+            .andExpect(status().isFound());
+    }
+
+    @Test
+    public void rendersErrorsOnInvalidAmount() throws Exception {
+        mockMvc.perform(post("/checkouts")
+                .param("payment_method_nonce", "fake-valid-nonce")
+                .param("amount", "-1.00"))
+            .andExpect(status().isFound())
+            .andExpect(flash().attributeExists("errorDetails"));
+
+        mockMvc.perform(post("/checkouts")
+                .param("payment_method_nonce", "fake-valid-nonce")
+                .param("amount", "not_a_valid_amount"))
+            .andExpect(status().isFound())
+            .andExpect(flash().attributeExists("errorDetails"));
+    }
+
+    @Test
+    public void redirectsOnTransactionNotFound() throws Exception {
+        mockMvc.perform(post("/checkouts/invalid-transaction"))
+            .andExpect(status().isFound());
+    }
+
+    @Test
+    public void redirectsRootToNew() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isFound());
+    }
+}
